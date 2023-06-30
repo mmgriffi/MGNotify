3c2588fd8d62b67dcc7ddba56cd3bc63fc772873
mmgriffi <michaelmgriffin82@gmail.com>
2023-06-30 17:28:35 +0100

    Update getFolder_list.ps1

--- code/getFolder_list.ps1
+++ code/getFolder_list.ps1
@@ -1,11 +1,1 @@
-﻿$folders = (get-childitem  -path "f:\n1_2023\f\nebula\archive$"  -Directory)
 
-
-foreach ($folder in $folders)
-{
-    $myfolder = $folder.fullname
-
- 
-    write-host "robocopy $myfolder"
-
-}
