76819ac573d20fa8500befbb52717ef1a5a7fa1d
mmgriffi <michaelmgriffin82@gmail.com>
2023-06-30 17:26:55 +0100

    Add files via upload

---
+++ code/getFolder_list.ps1
@@ -0,0 +1,11 @@
+﻿$folders = (get-childitem  -path "f:\n1_2023\f\nebula\archive$"  -Directory)
+
+
+foreach ($folder in $folders)
+{
+    $myfolder = $folder.fullname
+
+ 
+    write-host "robocopy $myfolder"
+
+}
